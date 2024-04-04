37aa2d3922ac6c163f723ef3604e9279241ab85c
amedina12 <amedina12@hawk.iit.edu>
2024-04-04 04:56:24 -0500

    fixing js cascade

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").addEventListener("submit", function(event) {
-        event.preventDefault();
-
-        alert("Your application is under review. An email will be sent upon your approval or denial.");
-
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-
-        this.reset();
-
-        
-
-    });
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your application is under review. An email will be sent upon your approval or denial.");
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.reset();
+  });
 });
