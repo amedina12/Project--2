60f7172e56ea8d4ca96056e7495d487c506f9335
amedina12 <amedina12@hawk.iit.edu>
2024-04-04 03:49:15 -0500

    adding credit card image

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("form").addEventListener("submit", function(event) {
+        event.preventDefault();
 
+        alert("Your application is under review. An email will be sent upon your approval or denial.");
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+
+        this.reset();
+
+    });
+});
